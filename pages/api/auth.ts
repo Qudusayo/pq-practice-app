e058dc4d46f82232e4d5072858567554406afd01
Qudusayo <qqudusayo@gmail.com>
2023-01-05 23:20:33 +0100

    fix: moved tokens to firebase

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,27 +1,35 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import { v4 as uuid } from "uuid";
 import type { NextApiRequest, NextApiResponse } from "next";
-import * as fs from "fs";
+import axios from "axios";
 
 type Response = {
   valid: boolean;
 };
+
+interface ApiResponseType {
+  [key: string]: { token: string };
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>
 ) {
   let { token } = req.body;
-  let tokens = fs.readFileSync("./tokens.json").toString("utf8");
-  const valid = JSON.parse(tokens).find((tk: string) => tk === token);
-  let filteredTokens;
 
-  if (valid) {
-    filteredTokens = JSON.parse(tokens).filter((tk: string) => tk !== token);
-    fs.writeFileSync("./tokens.json", JSON.stringify(filteredTokens, null, 2));
+  let response: ApiResponseType = (
+    await axios.get(`${process.env.API_BASE_URI}/tokens.json`)
+  ).data;
 
-    return res.status(200).json({ valid });
-  } else {
-    return res.status(403).json({ valid });
+  const checkForExistingToken = Object.entries(response).find(
+    (node) => node[1].token === token
+  );
+
+  if (!checkForExistingToken) return res.status(403).json({ valid: false });
+  if (checkForExistingToken.length) {
+    await axios.delete(
+      `${process.env.API_BASE_URI}/tokens/` + checkForExistingToken[0] + ".json"
+    );
+
+    return res.status(200).json({ valid: true });
   }
+  return res.status(403).json({ valid: false });
 }
