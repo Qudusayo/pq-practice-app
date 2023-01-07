69c565d33f5731c2a4d575e5a27636e5bf9f2bd9
Qudusayo <qqudusayo@gmail.com>
2023-01-07 10:47:05 +0100

    feat: page change progress loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import { SwitchProvider } from "../context/shuffleContext";
-import "../styles/globals.scss";
+import NProgress from "nprogress";
+import { Router } from "next/router";
 import type { AppProps } from "next/app";
+
+import { SwitchProvider } from "../context/shuffleContext";
+
+import "../styles/globals.scss";
+import "../styles/Nprogress.scss";
+
+// Router Events
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
